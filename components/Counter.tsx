ce8d72de135835c963d09e70911b86e873db0fd8
dreamsnama2021-git <dreamsnama2021@gmail.com>
2026-05-12 17:02:04 +0530

    Fix spacing in 'Trusted by' text

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -271,7 +271,7 @@ export default function CreativeStatsSection(): React.ReactElement {
             transition={{ delay: 0.1 }}
             className="ui-h1 font-extrabold capitalize tracking-tight mb-3 text-gray-900 mb-4"
           >
-            Trusted by <span className="text-[#0093cb]">Achieve Numbers</span>
+            Trusted by <span className="text-[#0093cb]"> Achieve Numbers </span>
           </motion.h2>
           
           <motion.p 
