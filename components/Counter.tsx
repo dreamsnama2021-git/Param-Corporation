88c0d3c52cd5393ab98d80a2e48db7b914079206
dreamsnama2021-git <dreamsnama2021@gmail.com>
2026-06-06 13:48:57 +0530

    new

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -312,7 +312,7 @@ export default function CreativeStatsSection(): React.ReactElement {
         </div>
 
         {/* Stats Grid - Staggered Layout */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-4 md:gap-5 lg:gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-4 md:gap-5 lg:gap-6">
           {stats.map((stat, index) => (
             <TiltCard key={stat.id} stat={stat} index={index} />
           ))}
