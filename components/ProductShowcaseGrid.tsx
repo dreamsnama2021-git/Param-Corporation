c9dd14648c15d0faae94b2ffaa0ca37d5344cbc1
dreamsnama2021-git <dreamsnama2021@gmail.com>
2026-09-07 17:56:52 +0530

    Update images for product showcases

--- components/ProductShowcaseGrid.tsx
+++ components/ProductShowcaseGrid.tsx
@@ -64,7 +64,7 @@ export default function ProductShowcaseGrid() {
       link: "/digital-gifts",
       bgClass: "from-[#031d2e] via-[#062c44] to-[#083c5e]",
       accentColor: "#0093cb",
-      desktopImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Digital.png",
+      desktopImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Digital%20Update.png",
       mobileImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Digital%20Update.png",
       tabName: "Digital"
     },
@@ -78,8 +78,8 @@ export default function ProductShowcaseGrid() {
       link: "/personalized",
       bgClass: "from-[#0f2419] via-[#143323] to-[#1a442e]",
       accentColor: "#00a65d",
-      desktopImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Personalized.png",
-      mobileImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Personalized.png",
+      desktopImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Peersonalized%20Update.png",
+      mobileImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Peersonalized%20Update.png",
       tabName: "Personalized"
     },
     {
@@ -106,7 +106,7 @@ export default function ProductShowcaseGrid() {
       link: "/koru",
       bgClass: "from-[#171412] via-[#241e1a] to-[#120f0d]",
       accentColor: "#0093cb",
-      desktopImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Koru.png",
+      desktopImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Koru%20update.png",
       mobileImage: "https://pub-735dbd7583d74ad5949115d6fdf77023.r2.dev/Banners/Circle%20Banner/Koru.png",
       tabName: "Detailers"
     }
